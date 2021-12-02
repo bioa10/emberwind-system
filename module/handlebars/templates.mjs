b2740f28d9729a576969bd36c956fd8cc71c8ff1
bioa10 <techguy1997@yahoo.com>
2021-12-01 20:59:53 -0600

    Added template for handlebars partials registration

---
+++ module/handlebars/templates.mjs
@@ -0,0 +1,34 @@
+/**
+ * Define a set of template paths to pre-load
+ * Pre-loaded templates are compiled and cached for fast access when rendering
+ *
+ * @returns {Promise}
+ */
+export const preloadHandlebarsTemplates = async function () {
+  // Define template paths to load
+  const templatePaths = [
+    // Actor Sheet Partials
+    "systems/emberwind-system/templates/sheets/components/hero-actions.hbs",
+
+    // Item Sheet Partials
+
+
+    // Apps
+
+
+    // Chat
+
+
+    // Chat card partials
+
+
+    // Internal Rendering Partials
+
+
+    // Tooltip
+
+  ];
+
+  // Load the template parts
+  return loadTemplates(templatePaths);
+};
