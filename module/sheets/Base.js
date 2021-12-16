bad26e390a5237c052dce6c60b117c19b8051ec4
ChibiBree <cheebibree@gmail.com>
2021-12-15 21:16:34 -0500

    seeing if this fixes the tab content

--- module/sheets/Base.js
+++ module/sheets/Base.js
@@ -1,30 +1,24 @@
-const template_file = "macro_data/TEMPLATE_FILE";
-const template_data = { header: "Handlebars header text.",
-                        tabs: [{ label: "Action",
-                                 title: "My First Tab",
-                                 content: "<em>Fancy tab1 content.</em>"},
-
-                               { label: "Tab2",
-                                 title: "My Second Tab",
-                                 content: "<em>Fancy tab2 content.</em>"}],
-                        footer: "Handlebars footer text."};
-const rendered_html = await renderTemplate(template_file, template_data);
-console.log(rendered_html);
-
-// Works as of Foundry 0.8.7; did not work in Foundry 0.7.9
-let d = new Dialog({
-    title: "MyDialogTitle",
-    content: rendered_html,
-    buttons: {
-        toggle: {
-            icon: '<i class="fas fa-check"></i>',
-            label: "Okay",
-            callback: () => console.log("Okay")
-        },
-    },
-    default: "toggle",
-    close: html => {
-        console.log(html);
-    },
-  }, { tabs: [{ navSelector: ".tabs", contentSelector: ".content", initial: "tab1" }]} );
-d.render(true);
+class myFormApplication extends FormApplication {
+    constructor(object, options) {
+      super(object, options);
+    }
+  
+    static get defaultOptions() {
+      return super.defaultOptions;
+    }
+  
+    getData(options = {}) {
+      return super.getData().object; // the object from the constructor is where we are storing the data
+    }
+  
+    activateListeners(html) {
+      super.activateListeners(html);
+    }
+  
+    async _updateObject(event, formData) {
+      return;
+    }
+  }
+  const my_form = new myFormApplication(template_data, { template: template_file,
+    tabs: [{navSelector: ".tabs", contentSelector: ".content", initial: "tab1"}] }); // data, options
+const res = await my_form.render(true);
