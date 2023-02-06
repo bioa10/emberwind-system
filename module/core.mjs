6f48f618cefb75d4eeb1e37ea012cc1bc7658744
ronman236 <rgoennier@gmail.com>
2023-02-05 18:15:23 -0600

    Began working on actor sheets, have a base layout with clickable tabs

--- module/core.mjs
+++ module/core.mjs
@@ -1,10 +1,11 @@
-import HeroSheet from "./sheets/HeroSheet.mjs";
+import HeroSheet from "./sheets/HeroSheet.js";
 import { preloadHandlebarsTemplates } from "./handlebars/templates.mjs"
 
 Hooks.once("init", function() {
   console.log("Initializing Emberwind System");
-  preloadHandlebarsTemplates();
 
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("hero", HeroSheet, { makeDefault: true });
+
+  return preloadHandlebarsTemplates();
 });
